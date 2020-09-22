2f28ef23ca8d01ce5d84e3af2ebaca774eb8f0c3
Sara Bastian <seb118@georgetown.edu>
2020-09-22 16:51:29 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,17 @@
 
 def key_for_min_value(name_hash)
 
+lowest_value = nil 
+lowest_key = nil 
+
+name_hash.each do |key, value|
+if lowest_value == nil
+  lowest_value = value
+  lowest_key = key
+elsif value < lowest_value
+  lowest_value = value
+  lowest_key = key
+end
+end
+lowest_key
 end
