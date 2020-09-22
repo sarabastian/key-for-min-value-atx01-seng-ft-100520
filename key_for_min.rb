93b318e0fb8e60307ee0e80bd1a5c3b3071c6575
Sara Bastian <seb118@georgetown.edu>
2020-09-22 16:49:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,11 @@ def key_for_min_value(name_hash)
 
 name_hash.each do |key, value|
 
-if value < lowest_value
+
+if lowest_value == nil
   lowest_value = value
   lowest_key = key
-elsif lowest_value == nil
+  elsif value < lowest_value
   lowest_value = value
   lowest_key = key
 end
